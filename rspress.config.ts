3383d53bbdbea433efafcb768ff42832173166c7
Junian Triajianto <author@junian.dev>
2025-08-29 15:48:47 +0700

    Fix base

--- rspress.config.ts
+++ rspress.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'rspress/config';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
-  base: '/rspress/',
+  base: '/rspress-site/',
   title: 'My Site',
   icon: '/rspress-icon.png',
   logo: {
